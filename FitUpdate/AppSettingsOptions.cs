d3e6b0f4c9158dd452fbb248240ae94bdb66ae40
lujiachao <632849093@qq.com>
2019-04-28 20:25:12 +0800

    添加项目文件。

---
+++ FitUpdate/AppSettingsOptions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitUpdate
+{
+    public class AppSettingsOptions
+    {
+        public string GuidRequest { get; set; }
+
+        public int Code { get; set; }
+
+        public string Message { get; set; }
+
+       public Data Data { get; set; }
+    }
+
+    public class Data
+    {
+        public string AgreeStatus { get; set; }
+
+        public string ExtendParam { get; set; }
+    }
+}
---
+++ FitUpdate/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace FitUpdate
+{
+    public class Program
+    {
+        public static List<Uri> _consulUri = new List<Uri>();
+        
+        public static void Main(string[] args)
+        {
+            CreateWebHostBuilder(args).Run();
+        }
+
+        public static IWebHost CreateWebHostBuilder(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(cb => 
+            {
+                _consulUri.Add(new Uri("http://localhost:8500"));
+                var configuration = cb.Build();
+                //cb.AddConsul(new[] { configuration.GetValue<Uri>("CONSUL_URL") }, configuration.GetValue<string>("CONSUL_PATH"));
+                cb.AddConsul(_consulUri, "key1");
+            })
+            .UseStartup<Startup>()
+            .Build();
+    }
+}
